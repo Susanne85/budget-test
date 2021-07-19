d5348cf32f30071fdb2f89e8a6d1921f2516d188
Susanne85 <xkqk2010@gmail.com>
2021-07-19 09:35:30 +0800

    Update files

---
+++ public/index.js
@@ -0,0 +1,182 @@
+const { useIndexedDB } = require("./indexDB");
+
+let transactions = [];
+let myChart;
+//Check if user is online and MONGO database can be updated from cache
+
+if (window.navigator.onLine) {
+  //Check the Cache
+  useIndexedDB('budget', 'budget', 'get', '').then(results => {
+    results.forEach(budgetCache => {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(budgetCache.budgetData),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => {
+          useIndexedDB('budget', 'budget', 'clear', budgetCache).then(results => {
+            console.log('Record cleared from INDEXDB');
+          });
+        })
+        .catch(err => {
+        });
+    });
+  });
+}
+
+fetch("/api/transaction")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log('Index JS - fetch data from Mongo', data);
+    transactions = data;
+
+    populateTotal();
+    populateTable();
+    populateChart();
+  });
+
+function populateTotal() {
+  // reduce transaction amounts to a single total value
+  let total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
+
+  let totalEl = document.querySelector("#total");
+  totalEl.textContent = total;
+}
+
+function populateTable() {
+  let tbody = document.querySelector("#tbody");
+  tbody.innerHTML = "";
+
+  transactions.forEach(transaction => {
+    // create and populate a table row
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${transaction.name}</td>
+      <td>${transaction.value}</td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+}
+
+function populateChart() {
+  // copy array and reverse it
+  //const test = {'name':'coffee','value':10,'Date1':'2021-07015'};
+  //useIndexedDB('budget','budget','put', test); 
+  let reversed = transactions.slice().reverse();
+  let sum = 0;
+
+  // create date labels for chart
+  let labels = reversed.map(t => {
+    let date = new Date(t.date);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  });
+
+  // create incremental values for chart
+  let data = reversed.map(t => {
+    sum += parseInt(t.value);
+    return sum;
+  });
+
+  // remove old chart if it exists
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  let ctx = document.getElementById("myChart").getContext("2d");
+
+  myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels,
+      datasets: [{
+        label: "Total Over Time",
+        fill: true,
+        backgroundColor: "#6666ff",
+        data
+      }]
+    }
+  });
+}
+
+function sendTransaction(isAdding) {
+  let nameEl = document.querySelector("#t-name");
+  let amountEl = document.querySelector("#t-amount");
+  let errorEl = document.querySelector(".form .error");
+
+  // validate form
+  if (nameEl.value === "" || amountEl.value === "") {
+    errorEl.textContent = "Missing Information";
+    return;
+  }
+  else {
+    errorEl.textContent = "";
+  }
+
+  // create record
+  let transaction = {
+    name: nameEl.value,
+    value: amountEl.value,
+    date: new Date().toISOString()
+  };
+
+  // if subtracting funds, convert amount to negative number
+  if (!isAdding) {
+    transaction.value *= -1;
+  }
+
+  // add to beginning of current array of data
+  transactions.unshift(transaction);
+
+  // re-run logic to populate ui with new record
+  populateChart();
+  populateTable();
+  populateTotal();
+
+  fetch("/api/transaction", {
+    method: "POST",
+    body: JSON.stringify(transaction),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      }
+      else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
+
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
+    });
+}
+function saveRecord(transaction) {
+  useIndexedDB('budget', 'budget', 'put', transaction);
+}
+document.querySelector("#add-btn").onclick = function () {
+  sendTransaction(true);
+};
+
+document.querySelector("#sub-btn").onclick = function () {
+  sendTransaction(false);
+};
---
+++ public/indexDB.js
@@ -0,0 +1,42 @@
+const { v4: uuidv4 } = require('uuid');
+function useIndexedDB(databaseName, storeName, method, object) {
+
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open(databaseName, 1);
+        let db, store;
+
+        request.onupgradeneeded = ({ event }) => {
+            db = request.result;
+            db.createObjectStore(storeName, { keyPath: "_id" });
+        };
+
+        request.onerror = (event) => {
+            console.log('IndexDB had an error', event);
+          };
+        request.onsuccess = (event) => {
+            //Setup a transaction store
+            db = event.target.result;
+            
+            const budgetTransaction = db.transaction([storeName], "readwrite");
+            store = budgetTransaction.objectStore(storeName);
+
+            if (method === "put") {
+               store.put({_id:uuidv4(), budgetData: object});
+            }
+            if (method === "clear") {
+                store.clear(object);
+            }
+            if (method === "get") {
+                const all = store.getAll();
+                all.onsuccess = function () {
+                    resolve(all.result);
+                };
+            }
+            budgetTransaction.oncomplete = function () {
+                db.close();
+            };
+        };
+    });
+}
+
+ module.exports = {useIndexedDB}
